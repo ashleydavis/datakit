0ddfa05393c3503b448e9580f7309897753df83e
Ashley Davis <ashley@codecapers.com.au>
2023-05-01 16:42:36 +1000

    Prototype of writing formatted documentation to the console for the map command.

--- src/cli/map.ts
+++ src/cli/map.ts
@@ -15,10 +15,59 @@ export async function main(argv: ParsedArgs): Promise<void> {
     outputJson(transformed);
 }
 
-if (require.main === module) {
-    run(main);
-}
+export const documentation = {
+    name: "map",
+    desc: "Creates an output dataset by calling the transformer function on every record of the input dataset.",
+    syntax: "map <input-file> <transformer-fn> [<output-file>]",
+    inputs: [
+        "JSON file",
+        "CSV file", 
+        "Yaml file",
+        "JSON formatted array on standard input.",
+    ],
+    outputs: [
+        "JSON file",
+        "CSV file", 
+        "Yaml file",
+        "JSON formatted array on standard output.",
+    ],
+    arguments: [
+        {
+            name: "input-file",
+            desc: "Can be an input file name (json, csv or  yaml) or a hypen to indicate reading JSON data from standard input.",
+        },
+        {
+            name: "transformer-fn",
+            desc: "A JavaScript function to transform each record of the input dataset. Specifying a file name will load the JavaScript from the file.",
+        },
+        {
+            name: "output-file",
+            desc: "The name of a file (json, csv or yaml) to output the resulting dataset to. Omitting this causes the output to written to standard output instead of a file.",
+        }
+    ],
+    examples: [
+        {
+            name: "Reads from standard input, applies the transformation and writes to standard output",
+            cmd: 'map - "r => r.x"',
+        },
+        {
+            name: "Reads from a file, applies the transformation and writes to standard output",
+            cmd: 'map input-file.csv "r -> r.x"',
+        },
+        {
+            name: "Reads from a file, applies the transformation and writes output to another file",
+            cmd: 'map input-file.csv "r -> r.x" output-file.csv'
+        },
+        {
+            name: "Loads a JavaScript file for the transformation",
+            cmd: 'map - my-transformation.js',
+        },
+    ],
+};
 
+if (require.main === module) {
+    run(main, documentation);
+}
 
 /*
 Help output:
@@ -56,13 +105,13 @@ JSON file, CSV file, Yaml file or JSON formatted array on standard output.
 map - "r => r.x" 
 ```
 
-### Reads from a file, applies the transform and writes to standard output:
+### Reads from a file, applies the transformation and writes to standard output:
 
 ```bash
 map input-file.csv "r -> r.x"
 ```
 
-### Reads from a file, applies the transform and writes output to another file
+### Reads from a file, applies the transformation and writes output to another file
 
 ```bash
 map input-file.csv "r -> r.x" output-file.csv
--- src/lib/command.ts
+++ src/lib/command.ts
@@ -1,12 +1,82 @@
 import minimist, { ParsedArgs } from "minimist";
 import "../cli/lib/load-globals";
+import chalk from 'chalk';
+
+export interface IDocumentation {
+    name: string;
+    desc: string;
+    syntax: string;
+    inputs: string[];
+    outputs: string[];
+    arguments: {
+        name: string;
+        desc: string;
+    }[];
+    examples: {
+        name: string;
+        desc?: string;
+        cmd: string;
+    }[];
+}
+
+//
+// Indents the input string by the requested amount.
+//
+function indent(input: string, indent: string): string {
+    return input.split("\n").map(line => indent + line).join("\n");
+}
+
+//
+// Displays formatted documentation.
+//
+function displayDocumentation(doco: IDocumentation): void {
+    console.log();
+    console.log(chalk.blueBright(doco.name));
+    console.log();
+    console.log(doco.desc);
+    console.log();
+    console.log("Syntax:");
+    console.log();
+    console.log(indent(doco.syntax, "\t"));
+    console.log();
+    console.log("Inputs (one of):");
+    console.log();
+    console.log(indent(doco.inputs.map(input => "- " + input).join("\n"), "\t"));
+    console.log();
+    console.log("Outputs (one of):");
+    console.log();
+    console.log(indent(doco.outputs.map(input => "- " + input).join("\n"), "\t"));
+    console.log();
+    console.log("Arguments:");
+    console.log();
+    console.log(indent(doco.arguments.map(input => `- ${chalk.blue(input.name)}: ${input.desc}`).join("\n"), "\t"));
+    console.log();
+    console.log("Examples:");
+    console.log();
+    for (const example of doco.examples) {
+        console.log(indent(example.name, "\t"));
+        if (example.desc) {
+            console.log();
+            console.log(indent(example.desc, "\t\t"));
+        }
+
+        console.log();
+        console.log(indent(chalk.green(example.cmd), "\t\t"));
+        console.log();
+    }
+    console.log();
+}
 
 //
 // Parses args before invoking a command then handling errors.
 //
-export async function run(invoke: (argv: ParsedArgs) => Promise<void>): Promise<void> {
+export async function run(invoke: (argv: ParsedArgs) => Promise<void>, doco: IDocumentation): Promise<void> {
 
     const argv = minimist(process.argv.slice(2));
+    if (argv.h || argv.help || (argv._.length > 0 && argv._[0] === "help")) {
+        displayDocumentation(doco)
+        process.exit(2);
+    }
 
     try {
         await invoke(argv);
