3a23d9fd253d55258027e65314585c0cf990929b
Ashley Davis <ashley@codecapers.com.au>
2023-05-05 18:45:44 +1000

    Factored out the function that invokes a user function and reports errors.

--- src/cli/lib/user-fn.ts
+++ src/cli/lib/user-fn.ts
@@ -2,11 +2,29 @@ import * as path from "path";
 import * as fs from "fs";
 import { isFunction } from "../../lib/utils";
 import { consumeOptionalArg } from "./args";
+import chalk from "chalk";
+
+export interface IUserFn {
+    //
+    // User defined function to be invoked for data transformation.
+    //
+    fn: Function;
+
+    //
+    // Function to lazy load source code error reporting.
+    //
+    loadSourceCode: () => string;
+    
+    //
+    // The name of the file.
+    //
+    fileName: string;
+}
 
 //
 // Loads a user function.
 //
-export function loadUserFn(argv: string[], exampleFn: string): { userFn: Function, loadSourceCode: () => string, fileName?: string } {
+export function loadUserFn(argv: string[], exampleFn: string): IUserFn {
 
     let userFn: any;
 
@@ -34,8 +52,60 @@ export function loadUserFn(argv: string[], exampleFn: string): { userFn: Functio
     }
 
     return {
-        userFn,
+        fn: userFn,
         loadSourceCode,
         fileName,
     };
+}
+
+//
+// Invokes a user function and handlers errors.
+//
+export function invokeUserFn(userFn: IUserFn): void {
+    try {
+        return userFn.fn();
+    }
+    catch (err: any) {
+        if (err.stack) {
+            const matches = (new RegExp(`${userFn.fileName}:(\\d+):(\\d+)`)).exec(err.stack);
+            if (matches) {
+
+                const message = err.message || firstLine(err.stack);
+                const lineNumber = parseInt(matches[1]);
+                const columnNumber = parseInt(matches[2]);
+
+                console.error(chalk.red(`Error: `) + message);
+                console.error(` ${chalk.cyan("-->")} ${userFn.fileName}:${lineNumber}:${columnNumber}`)
+
+                const sourceCode = userFn.loadSourceCode();
+                const sourceCodeLines = sourceCode.split("\n");
+                console.error();
+                console.error(sourceCodeLines.slice(Math.max(0, lineNumber-3), lineNumber-1).map(line => "    " + line).join("\n"));
+                console.error(`${chalk.red("  > ")}${sourceCodeLines[lineNumber-1]}`);
+                console.error(" ".repeat(columnNumber-1) + chalk.red("    ^ "  + message));
+                console.error(sourceCodeLines.slice(lineNumber, lineNumber + 3).map(line => "    " + line).join("\n"));
+
+                console.error();
+                console.error(err.stack.split("\n").map((line: string) => "  " + line).join("\n"));
+                console.error();
+
+                throw new Error(`Runtime error from user function`);
+            }
+        }
+        
+        throw err;
+    }
+}
+
+//
+// Extracts the first line of the input string and return it.
+//
+function firstLine(input: string) {
+    const newlineIndex = input.indexOf("\n");
+    if (newlineIndex > 0) {
+        return input.substring(0, newlineIndex).trimEnd();
+    }
+    else {
+        return input.trimEnd();
+    }    
 }
--- src/cli/map.ts
+++ src/cli/map.ts
@@ -1,63 +1,21 @@
 import { inputJson, outputJson } from "../lib/io";
-import { loadUserFn } from "./lib/user-fn";
+import { invokeUserFn, loadUserFn } from "./lib/user-fn";
 import { verifyInputArray } from "../lib/verify";
 import { run } from "../lib/command";
-import chalk from "chalk";
-
-//
-// Extracts the first line of the input string and return it.
-//
-function firstLine(input: string) {
-    const newlineIndex = input.indexOf("\n");
-    if (newlineIndex > 0) {
-        return input.substring(0, newlineIndex).trimEnd();
-    }
-    else {
-        return input.trimEnd();
-    }    
-}
 
 export async function main(argv: string[]): Promise<void> {
 
-    const { userFn, loadSourceCode, fileName } = loadUserFn(argv, `r => transform(r)`);
+    const { fn, loadSourceCode, fileName } = loadUserFn(argv, `r => transform(r)`);
     const data = await inputJson();
     verifyInputArray(data, "map");
 
-    try {
-        const transformed = data.map(userFn);
-        outputJson(transformed);
-    }
-    catch (err: any) {
-        if (err.stack) {
-            const matches = (new RegExp(`${fileName}:(\\d+):(\\d+)`)).exec(err.stack);
-            if (matches) {
+    const transformed = invokeUserFn({
+        fn: () => data.map(fn),
+        loadSourceCode,
+        fileName,
+    });
 
-                const message = err.message || firstLine(err.stack);
-                const lineNumber = parseInt(matches[1]);
-                const columnNumber = parseInt(matches[2]);
-
-                console.error(chalk.red(`Error: `) + message);
-                console.error(` ${chalk.cyan("-->")} ${fileName}:${lineNumber}:${columnNumber}`)
-
-                const sourceCode = loadSourceCode();
-                const sourceCodeLines = sourceCode.split("\n");
-                console.error();
-                console.error(sourceCodeLines.slice(Math.max(0, lineNumber-3), lineNumber-1).map(line => "    " + line).join("\n"));
-                console.error(`${chalk.red("  > ")}${sourceCodeLines[lineNumber-1]}`);
-                console.error(" ".repeat(columnNumber-1) + chalk.red("    ^ "  + message));
-                console.error(sourceCodeLines.slice(lineNumber, lineNumber + 3).map(line => "    " + line).join("\n"));
-
-                console.error();
-                console.error(err.stack.split("\n").map((line: string) => "  " + line).join("\n"));
-                console.error();
-
-                throw new Error(`Runtime error from user function`);
-            }
-        }
-        console.log(typeof(err.stack));
-        throw err;
-    }
-
+    outputJson(transformed);
 }
 
 export const documentation = {
