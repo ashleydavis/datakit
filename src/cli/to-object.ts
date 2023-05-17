a04c195869f1749cec5a57b97e879ed689150c97
Ashley Davis <ashley@codecapers.com.au>
2023-05-17 17:15:28 +1000

    Rearranged to-object command arguments to a better order.

--- src/cli/to-object.ts
+++ src/cli/to-object.ts
@@ -5,11 +5,11 @@ import { standardCmdInputs, standardInputFileHelp, standardOutputFileHelp } from
 
 export async function main(argv: string[]): Promise<void> {
 
-    const data = await inputData(argv);
-    verifyInputArray(data, "to-object");
-
     const keySelectorFn = loadUserFn(argv, `r => r.key`);
     const valueSelectorFn = loadUserFn(argv, `r => r.value`);
+
+    const data = await inputData(argv);
+    verifyInputArray(data, "to-object");
 
     const reduced = data.reduce((a: any, r: any) => {
         const key = invokeUserFn(() => keySelectorFn.fn(r), keySelectorFn.details);
@@ -24,7 +24,7 @@ export async function main(argv: string[]): Promise<void> {
 export const documentation = {
     name: "to-object",
     desc: "Creates a JSON object from key/value pairs extracted from the input dataset.",
-    syntax: "to-object <input-file> <key-selector-fn> <value-selector-fn> [<output-file>]",
+    syntax: "to-object <key-selector-fn> <value-selector-fn> [<input-file>] [<output-file>]",
     inputs: standardCmdInputs,
     outputs: [
         "JSON file",
@@ -32,7 +32,6 @@ export const documentation = {
         "JSON formatted data on standard output.",
     ],
     args: [
-        standardInputFileHelp,
         {
             name: "key-selector-fn",
             desc: "A JavaScript function to select the key from each record of the input dataset. Specifying a file name will load the JavaScript code from the file.",
@@ -41,24 +40,29 @@ export const documentation = {
             name: "value-selector-fn",
             desc: "A JavaScript function to select the value from each record of the input dataset. Specifying a file name will load the JavaScript code from the file.",
         },
+        standardInputFileHelp,
         standardOutputFileHelp,
     ],
     examples: [
         {
             name: "Reads JSON data from standard input and writes the JSON object to standard output",
-            cmd: 'command-that-produces-json | to-object - "r => r.key" "r => r.value"',
+            cmd: 'command-that-produces-json | to-object "r => r.key" "r => r.value"',
         },
         {
-            name: "Reads data from a file, applies the transformation and writes to standard output",
-            cmd: 'to-object input-file.csv "r => r.key" "r => r.value"',
+            name: "Reads data from a file and writes the JSON object to standard output",
+            cmd: 'to-object "r => r.key" "r => r.value"  input-file.csv',
         },
         {
-            name: "Reads data from a file, applies the transformation and writes output to another file",
-            cmd: 'to-object input-file.csv "r => r.key" "r => r.value" output-file.json'
+            name: "Reads data from a file and writes the JSON object to a file",
+            cmd: 'to-object "r => r.key" "r => r.value" input-file.csv output-file.json'
         },
         {
             name: "Loads JavaScript files for the key and value selector functions",
-            cmd: 'to-object input-file.csv my-key-selector.js my-value-selector.js',
+            cmd: 'to-object --file my-key-selector.js --file my-value-selector.js input-file.csv',
+        },
+        {
+            name: "Reads JSON data from standard inputand writes the JSON object to a file",
+            cmd: 'to-object "r => r.key" "r => r.value" - output-file.json'
         },
     ],
     notes: [
